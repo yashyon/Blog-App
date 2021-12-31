4c00b46a5c61eb4dd31d14507c1cb9a5ea8bf17a
yashyon <yashpradhan60@gmail.com>
2021-12-31 14:13:57 +0530

    Initial Commit

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const url = "https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=jeshoots-com-pUAM5hPaCRI-unsplash.jpg&w=1920";
     return (
         <div className={styles.container}>
-            <h1 className={styles.text}>BLOG WEBSITE</h1>
+            <h1 className={styles.text}>BLOG APP</h1>
             <img className={styles.image} src={url} alt='rkmbomb' />
         </div>
     )
